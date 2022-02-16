45cfd60dbf6f1549e593f13cec80f4b0829fd217
Meerim <eg.meerim@gmail.com>
2022-02-16 11:14:08 +0600

    feat: HW

--- 16.02.2022/script.js
+++ 16.02.2022/script.js
@@ -1,2 +1,14 @@
 // Camel case
+let camelCASE = 1;
+let PascalCse = 2;
+let snake_case =3;
+alert(`${camelCASE} ${PascalCse} ${snake_case}`);
+console.log (`${camelCASE} ${PascalCse} ${snake_case}`)
+
+
+//Задача 2
+let str1 = 'САЛАМ';
+let str2 = 'БИШКЕК!';
+alert(`${str1}, ${str2}`);
+console.log (`${str1}, ${str2}`)
 
