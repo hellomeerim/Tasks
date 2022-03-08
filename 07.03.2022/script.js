4dbcd1127cfb23aafe3b2241b3326cc7ad1dbf29
hellomeerim <eg.meerim@gmail.com>
2022-03-09 00:06:18 +0600

    feat: HW for 07.03.2022

---
+++ 07.03.2022/script.js
@@ -0,0 +1,26 @@
+/*
+1) Напишите функцию showNotification(options), которая создаёт уведомление:
+    <div class="notification"> с заданным содержимым.
+    Уведомление должно автоматически исчезнуть через 1,5 секунды.
+
+Пример объекта options:
+// показывает элемент с текстом "Hello" рядом с правой верхней частью окна.
+showNotification({
+html: "Hello!",      // HTML-уведомление
+className: "welcome" // дополнительный класс для div (необязательно)
+});*/
+
+let info = document.querySelector('.notification');
+
+function showNotification() {
+    info.classList.add('notification');
+    info.classList.remove('notification');
+}
+
+setTimeout(showNotification, 1500);
+
+setTimeout(function(){
+    document.getElementById('message').style.display = 'none';
+}, 1500);
+
+
